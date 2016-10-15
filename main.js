c88494a2624e3a5318896c5d70a3dcd3d660cd98
jelle van loock <r0484335@student.thomasmore.be>
2016-10-15 10:52:08 +0200

    3. GET request toegevoegd

--- main.js
+++ main.js
@@ -5,5 +5,9 @@ var bodyparser = require('body-parser');
 
 var app = express();
 
+app.get("/", function(request, response){
+    response.sendfile(__dirname + '/calculator.html');
+});
+
 app.listen(4567);
 console.log("Start server...")
