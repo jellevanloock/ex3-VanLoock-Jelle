c9a38ccd53dddc6b8ed2dab3532155133abdc136
jelle van loock <r0484335@student.thomasmore.be>
2016-10-15 11:03:27 +0200

    4. Zorg dat je HTML aangepast is voor een post te doen + urlencoded toegevoegd

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var bodyparser = require('body-parser');
 
 var app = express();
 
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on request.body
+ * Gezien bij Jonas Cristens
+ */
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
 app.get("/", function(request, response){
     response.sendfile(__dirname + '/calculator.html');
 });
