9fb8a7fa808d036742f7fa13b11f94db4ae697aa
jelle van loock <r0484335@student.thomasmore.be>
2016-10-17 11:11:40 +0200

    9. bodyparser.json toegevoegd (resultaat weergeven in json) extra get toegevoegd

--- main.js
+++ main.js
@@ -8,20 +8,31 @@ var app = express();
 /** bodyParser.urlencoded(options)
  * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
  * and exposes the resulting object (containing the keys and values) on request.body
- * Gezien bij Jonas Cristens
+ * Gezien bij Jonas Cristens: http://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
  */
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
+app.use(bodyparser.json());
+var uitkomst = [];
 
 //app.use(express.static(__dirname, "ex3-VanLoock-Jelle"));
 app.get("/", function(request, response){
     response.sendfile(__dirname + '/calculator.html');
 });
 
+//bijhouden welke berekingen er al uitgevoerd zijn
+app.get("/uitkomst", function(request, response){
+  //stuurt als antwoord de inhoud van onze database. Standaard in json terug gestuurd.
+  response.send(uitkomst);
+});
+
 app.post("/", function(request, response){
-    console.log(request.body.display);
+    //console.log(request.body.display);
+    var berekening = request.body.display;
     var uitrekenen = eval(request.body.display);
+    uitkomst.push({"berekening": berekening, "uitkomst": uitrekenen})
     console.log(uitrekenen);
     response.send('<h2>'+uitrekenen+'</h2>');
 })
